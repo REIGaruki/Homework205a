b3329b10fd494ce0ae0dbc8032615c811a3f70ce
REIGaruki <gyakujo@mail.ru>
2024-05-06 17:26:15 +0300

    Static method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    public static boolean confirmAllowedChars(String string) {
+        boolean confirmed = true;
+        if (string.toCharArray().length <= 20) {
+            String allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_";
+            for (char a : string.toLowerCase().toCharArray()) {
+                if (confirmed) {
+                    for (char b : allowedChars.toCharArray()) {
+                        if (a == b) {
+                            confirmed = true;
+                            break;
+                        }
+                        confirmed = false;
+                    }
+                }
+            }
+        } else {
+            confirmed = false;
+        }
+        return confirmed;
+    }
 }
